a2f3ed824257c2ca40112c50b6c01c634188e43c
robotbouba9 <robotbouba9@example.com>
2025-08-18 12:39:23 -0400

    Add system dependencies for WeasyPrint in install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install -y libpango-1.0-0 libpangoft2-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev libcairo2 libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 shared-mime-info
+
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
